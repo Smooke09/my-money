b66841f334d7b3bad015c275cdaaacc1d4d6c5b9
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-30 09:30:46 -0300

    Bug fixed token

--- src/api/user/authService.js
+++ src/api/user/authService.js
@@ -36,7 +36,6 @@ const login = (req, res, next) => {
 //validação
 const validateToken = (req, res, next) => {
   const token = req.body.token || "";
-
   jwt.verify(token, env.authSecret, function (err, decoded) {
     return res.status(200).send({ valid: !err });
   });
@@ -80,3 +79,5 @@ const signup = (req, res, next) => {
     }
   });
 };
+
+module.exports = { login, signup, validateToken };
--- src/config/auth.js
+++ src/config/auth.js
@@ -1,25 +1,24 @@
 const jwt = require("jsonwebtoken");
 const env = require("../.env");
-
 module.exports = (req, res, next) => {
-  // CORs preflight request
+  // CORS preflight request
   if (req.method === "OPTIONS") {
     next();
   } else {
     const token =
-      req.body.token || req.quey.token || req.headers["authorization"];
-
-    if (!token) return res.status(403).send({ erros: ["No token provided."] });
-
-    jwt.verify(token, env.authSecret, function(err,decoded){
-        if(err){
-            return res.status(403).send({
-                erros: ['Failed to authenticate token.']
-            })
-        }else{
-            // req.decoded = decoded
-            next()
-        }
-    })
+      req.body.token || req.query.token || req.headers["authorization"];
+    if (!token) {
+      return res.status(403).send({ errors: ["No token provided."] });
+    }
+    jwt.verify(token, env.authSecret, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({
+          errors: ["Failed to authenticate token."],
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
   }
 };
