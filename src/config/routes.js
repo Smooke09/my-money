47534dd1fbbefbb2a7233f83208cf3edef3ec476
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-30 09:13:54 -0300

    adding public routes and private routes by token

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,12 +1,24 @@
-const express = require('express')
+const express = require("express");
+const auth = require("./auth");
 
-module.exports = function(server) {
+module.exports = function (server) {
+  const protectedApi = express.Router();
+  server.use("/api", protectedApi);
 
-    // Definir URL base para todas as rotas 
-    const router = express.Router()
-    server.use('/api', router)
+  // rotas protegidas por Token JWT
+  //Passando pelo filtro de autenticação
+  protectedApi.use(auth);
 
-    // Rotas de Ciclo de Pagamento 
-    const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
-}
+  //Rota de ciclo de pagamentos dentro deela tem todos serviços criado
+  const BillingCylce = require("../api/billingCycle/billingCycleService");
+  BillingCylce.register(protectedApi, "/billingCycles");
+
+  //Rotas abertas
+  const openApi = express.Router();
+  server.use("/oapi", openApi);
+
+  const AuthService = require("../api/user/authService");
+  openApi.post("/login", AuthService.login);
+  openApi.post("/signup", AuthService.signup);
+  openApi.post("/validateToken", AuthService.validateToken);
+};
